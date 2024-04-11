5c44714856cc9b0804c1d7fb6ab1b324f269e7fd
frenzie24 <kevincharlesgross@gmail.com>
2024-04-11 13:59:18 -0400

    added functions to store tasks

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,7 +2,12 @@
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
-
+// all possible status for tasks
+const tStatus = {
+    todo: 'to-do',
+    prog: 'in-progress',
+    done: 'done',
+};
 
 
 // Todo: create a function to generate a unique task id
@@ -22,7 +27,38 @@ function renderTaskList() {
 
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
+    // prevent the modal from dismissing 
+    event.preventDefault();
+    // object to store our task's data
+    let task = {
+        // title, dueDate, and description all get their values from user input
+        title: $("#task-title").val(),
+        dueDate: dayjs(new Date($("#task-due-date").val())),
+        description: $("#task-description").val(),
+        // id is generated with by calling generateTaskId
+        id: generateTaskId(),
+        // Every new task will have its status set to to-do by default
+        status: tStatus.todo,
+    }
+    if (task.title == '' || !task.title) {
+        return;
+    }
+    console.log(`new task: ${task}`);
+    // use our toolBox function to store our task without thinknig about it
+    storeTask(task);
+    $('#taskModal').modal('hide');
+}
 
+function storeTask (task) {
+    // use toolBox function getItem to get our tasks wihtout worrying about parsing the data
+    let tasks = getItem('tasks');
+    // if tasks were ever stored incorrectly wipe it all and make a new array
+    if (!tasks || typeof(tasks)==!Array){
+        tasks = [];
+    }
+    tasks.push(task);
+       // use our toolBox function to store our task without worrying about parsing the data
+    setItem('tasks', tasks);
 }
 
 // Todo: create a function to handle deleting a task
@@ -37,5 +73,6 @@ function handleDrop(event, ui) {
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
-
+    // add click listener to add task btn
+    $('#add-task-btn').on('click', handleAddTask);
 });
